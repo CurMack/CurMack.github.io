a5b9bcf59183058da0650495e95c81ddc5394572
CurMack <103652762+CurMack@users.noreply.github.com>
2026-07-28 09:28:40 +0000

    Deploying to gh-pages from @ CurMack/CurMack.github.io@4d19e27fcf45b2784233e2728f37a95ee76eeb7e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "My publications by categories in reversed chronological order.",
+          description: "My publications by categories in reversed chronological order. Full list on Google Scholar.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
